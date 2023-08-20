4520efe02f2865f2c77c3c0d93f6c22394574f28
Mohsin Miyaji <mohsin.kmiyaji@gmail.com>
2023-08-20 16:57:49 +0530

    2nd commit

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.json({ mssg: "This is a successful GET req" });
 });
 
