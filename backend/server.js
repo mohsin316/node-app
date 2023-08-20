eec6b67ac00dbfe9bb5ed3be2faa49d58956bf6b
Mohsin Miyaji <mohsin.kmiyaji@gmail.com>
2023-08-20 16:26:57 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.json({ mssg: "This is a successful GET req" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("listening on port", process.env.PORT);
+});
